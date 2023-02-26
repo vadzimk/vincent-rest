1c2d956b175fd3c068bf290d18f045caaf24131d
Vadzim <vadzimk@hotmail.com>
2023-02-25 22:09:43 -0800

    add todo model tests

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
-# Create your tests here.
+from todo.models import Todo
+
+
+class TodoModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Todo.objects.create(title='first todo', body='the body')
+
+    def test_title_content(self):
+        todo = Todo.objects.get(id=1)
+        self.assertEqual(todo.title, 'first todo')
+
+    def test_body_content(self):
+        todo = Todo.objects.get(id=1)
+        self.assertEqual(todo.body, 'the body')
